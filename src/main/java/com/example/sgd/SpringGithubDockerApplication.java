d788bc0d1721c34f3d86a9b9aba0e670f8138686
CR <csernikr@gmail.com>
2021-07-30 21:33:43 +0200

    init

---
+++ src/main/java/com/example/sgd/SpringGithubDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.sgd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGithubDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGithubDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sgd/SpringGithubDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sgd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGithubDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
